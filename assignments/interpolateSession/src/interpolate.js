30457554f6026b69fbba27a65e7940e1ec503215
erdoganabaci <erdoganabaci97@gmail.com>
2021-12-24 02:04:21 +0100

    Complete interpolation assignment

--- assignments/interpolateSession/src/interpolate.js
+++ assignments/interpolateSession/src/interpolate.js
@@ -1,5 +1,25 @@
-const interpolate = (value, session = {}, options = {}) => {
-    //TODO
+const _ = require("lodash");
+
+const interpolate = (conversation, session = {}, options = {}) => {
+  // memorizing as a state
+  let result = conversation;
+  // test for an empty object
+  if (!_.isEmpty(session)) {
+    Object.entries(session).forEach(([key, value]) => {
+      let delimKey = `${options.leftDelimiter}${key}${options.rightDelimiter}`;
+      // search each key is contain our string
+      if (result.indexOf(`${delimKey}`) != -1) {
+        // If match each key in our string then replace your key with delims to options value
+        result = result.replace(`${delimKey}`, value);
+      }
+    });
+  } else if (options.leftDelimiter == "{" && options.rightDelimiter == "}") {
+    // regex referance here => https://javascript.info/regexp-anchors
+    // string regex remove https://stackoverflow.com/questions/14648375/javascript-regex-to-remove-string-inside-bracket
+    result = result.replace(/{[^}]*}/, "");
+  }
+
+  return result;
 };
 
-module.exports = { interpolate }
+module.exports = { interpolate };

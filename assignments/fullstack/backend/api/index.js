20771e341cca279f60f2b2c48051154925dac634
erdoganabaci <erdoganabaci97@gmail.com>
2021-12-28 01:59:37 +0100

    Httpserver for vercel

--- assignments/fullstack/backend/api/index.js
+++ assignments/fullstack/backend/api/index.js
@@ -6,10 +6,13 @@ const { SubscriptionServer } = require("subscriptions-transport-ws");
 const { makeExecutableSchema } = require("@graphql-tools/schema");
 const { typeDefs, dateScalar } = require("./typedef");
 const resolvers = require("./resolver");
+require("dotenv").config();
 
 let server;
 const app = express();
 const httpServer = createServer(app);
+
+const PORT = process.env.PORT || 4040;
 
 // graphql doesnt support scalar type such as Date so we need to implement external https://hasura.io/blog/working-with-dates-time-timezones-graphql-postgresql/
 const schema = makeExecutableSchema({
@@ -34,5 +37,14 @@ const startApolloServer = async (app, httpServer) => {
 };
 
 startApolloServer(app, httpServer);
+
+httpServer.listen(PORT, () => {
+  console.log(
+    `🚀 Query endpoint ready at http://localhost:${PORT}${server.graphqlPath}`
+  );
+  console.log(
+    `🚀 Subscription endpoint ready at ws://localhost:${PORT}${server.graphqlPath}`
+  );
+});
 
 module.exports = { httpServer, server };

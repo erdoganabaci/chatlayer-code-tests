f432786ec4479f88c013b69ad060216f422dc5c8
erdoganabaci <erdoganabaci97@gmail.com>
2021-12-28 23:21:28 +0100

    Activate Landing Page

--- assignments/fullstack/backend/api/index.js
+++ assignments/fullstack/backend/api/index.js
@@ -29,7 +29,7 @@ const schema = makeExecutableSchema({
 const startApolloServer = async (app, httpServer) => {
   server = new ApolloServer({
     schema,
-    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+    plugins: [ApolloServerPluginLandingPageLocalDefault()],
   });
   await server.start();
   server.applyMiddleware({ app, path: "/api/graphql" });
@@ -42,13 +42,13 @@ const startApolloServer = async (app, httpServer) => {
 
 startApolloServer(app, httpServer);
 
-httpServer.listen(PORT, () => {
-  console.log(
-    `🚀 Query endpoint ready at http://localhost:${PORT}${server.graphqlPath}`
-  );
-  console.log(
-    `🚀 Subscription endpoint ready at ws://localhost:${PORT}${server.graphqlPath}`
-  );
-});
+// httpServer.listen(PORT, () => {
+//   console.log(
+//     `🚀 Query endpoint ready at http://localhost:${PORT}${server.graphqlPath}`
+//   );
+//   console.log(
+//     `🚀 Subscription endpoint ready at ws://localhost:${PORT}${server.graphqlPath}`
+//   );
+// });
 
 module.exports = { httpServer, server };
